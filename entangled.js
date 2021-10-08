fa62cf658d4678c45b2c16ee7e23042edbac8f24
TanviMishra <misht490@newschool.edu>
2021-10-07 22:12:55 -0400

    Update entangled.js

--- entangled.js
+++ entangled.js
@@ -1,13 +1,14 @@
 function startEnt(){
     //randomly assigns some mines as entangled mines
     RandCell=grid[random(cols)][random(rows)];
-    let entMine = [];
-    while(entMineCount<int(cols*rows*entMineRatio) && RandCell.mine==true && RandCell.entMine==false){
+    let entMineArray = [];
+    while(entMineCount<int(cols*rows*entMineRatio) && (RandCell.mine==true) && (RandCell.entMine==false)){
         RandCell.entMine==true;
-        entMine.push(RandCell);
+        entMineArray.push(RandCell);
         entMineCount++;
 
     }
+    console.log(entMineCount)
 
     // break down entangled mine number into an array of 2s and 3s
     // take the array of entangled mines and use the 2/3 array to assign pairs
